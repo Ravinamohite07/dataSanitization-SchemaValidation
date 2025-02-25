4490f1d62c4857971850044baf31542fee4d1d7a
Ravinamohite07 <ravinamohite527@gmail.com>
2025-02-25 23:31:35 +0530

    api level sanitization

--- src/app02.js
+++ src/app02.js
@@ -81,6 +81,9 @@ app.patch("/user/:userId",async (req,res) => {
         if(!isUpdateAllowed){
             throw new Error("update is not allowed..!");
         }
+        if(data?.Skills.length > 10){
+            throw new Error("Skills cannot be more than 10");
+        }
         const user = await User.findByIdAndUpdate({_id: userId},data,
             {returnDocument:"after",runValidators: true,});
             //{returnDocument:"before"}
